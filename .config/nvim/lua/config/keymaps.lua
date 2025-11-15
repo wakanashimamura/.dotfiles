7a696c6730bcc5507061b076b3f0c5eb0835680a
Wakana Shimamura <158538120+wakanashimamura@users.noreply.github.com>
2025-11-15 03:31:34 +0100

    feat(nvim): add lazy plugin manager and initial plugins
    Added the following plugins:
    - catppuccin/nvim
    - mini.move
    - neo-tree.nvim
    - nvim-lsp-file-operations
    - nvim-window-picker
    
    Removed custom line-moving mappings in favor of mini.move.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -57,17 +57,6 @@ map("n", "<C-Left>",  "<Cmd>vertical resize -2<CR>", { desc = "Decrease window w
 map("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", { desc = "Increase window width"  })
 
 -- ┌────────────────────────────────────────────────────────────────┐
--- │                           MOVE LINES                           │
--- └────────────────────────────────────────────────────────────────┘
-
-map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==",                   {desc="Move Down"})
-map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==",             {desc="Move Up"  })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi",                                   {desc="Move Down"})
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi",                                   {desc="Move Up"  })
-map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv",       {desc="Move Down"})
-map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", {desc="Move Up"  })
-
--- ┌────────────────────────────────────────────────────────────────┐
 -- │                             BUFFER                             │
 -- └────────────────────────────────────────────────────────────────┘
 
---
+++ .config/nvim/lua/plugins/neo_tree.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>e", ":Neotree toggle reveal left<CR>", { desc = "Toggle Neotree" })
+		  vim.keymap.set("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus Neotree" })
+		  vim.keymap.set("n", "<leader>g", ":Neotree git_status float<CR>", { desc = "Neotree Git status" })
+		  vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal float<CR>", { desc = "Neotree buffers" })
+      require("neo-tree").setup({
+        filesystem = { 
+          follow_current_file = { enabled = true },
+        },
+        event_handlers = {
+          {
+            event = "neo_tree_buffer_enter",
+            handler = function()
+              vim.wo.relativenumber = true
+            end,
+          }
+        },
+      })
+    end,
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim", 
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup({
+        hint = 'floating-big-letter',
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          bo = {
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+      })
+    end,
+  },
+}
