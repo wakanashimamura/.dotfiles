d5060112c96cf85b4cfc30c9a635130786b48ae3
Wakana Shimamura <158538120+wakanashimamura@users.noreply.github.com>
2025-11-17 18:29:44 +0100

    feat(nvim): add plugins: bufferline.nvim, gitsigns.nvim, indent-blankline.nvim

---
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -0,0 +1,17 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    require("ibl").setup({
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { 
+        enabled    = true,
+        show_start = false,
+        show_end   = false,
+      }
+    })
+  end
+}
